5526c0366069f6aed7cb7ecd1de8c72de5206a4f
Bohdan Ponamariov <bodiakpi1992@gmail.comm>
2022-07-26 08:45:44 +0300

    optimization scss

--- src/js/components/burgerMenu.js
+++ src/js/components/burgerMenu.js
@@ -5,29 +5,9 @@ $menuTrigger = $('.menuTrigger');
 $menuTrigger.on('click', () => {
 	if ($('body').hasClass('menu_open')) {
 		$('body').removeClass('menu_open');
-		$(this).removeClass('active_mod');
+		$('.menuTrigger').removeClass('active_mod');
 	} else {
 		$('body').addClass('menu_open');
-		$(this).addClass('active_mod');
+		$('.menuTrigger').addClass('active_mod');
 	}
 });
-
-const cardBody = document.querySelectorAll('.header_menu');
-const planItem = document.querySelectorAll('.header_menu__item');
-
-function addClass(target) {
-	if (target.closest('.menu_trigger') || target.classList.contains('header_menu__item')) {
-		target.closest('.menu_trigger').classList.toggle('active_mod');
-	}
-}
-
-function select(e) {
-	const targetElement = e.target;
-	if (planItem) {
-		planItem.forEach(card => {
-			addClass(targetElement);
-		});
-	}
-}
-
-document.addEventListener('click', select);
