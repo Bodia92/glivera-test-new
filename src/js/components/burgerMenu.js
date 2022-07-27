13a6b676a2f665ef0bee590d7581d952f62a8abe
Bohdan Ponamariov <bodiakpi1992@gmail.comm>
2022-07-27 23:34:59 +0300

    fix scss header

--- src/js/components/burgerMenu.js
+++ src/js/components/burgerMenu.js
@@ -3,11 +3,11 @@ let $menuTrigger;
 $menuTrigger = $('.menuTrigger');
 
 $menuTrigger.on('click', () => {
-	if ($('body').hasClass('menu_open')) {
-		$('body').removeClass('menu_open');
+	if ($('body').hasClass('body--open_menu_state')) {
+		$('body').removeClass('body--open_menu_state');
 		$('.menuTrigger').removeClass('active_mod');
 	} else {
-		$('body').addClass('menu_open');
+		$('body').addClass('body--open_menu_state');
 		$('.menuTrigger').addClass('active_mod');
 	}
 });
